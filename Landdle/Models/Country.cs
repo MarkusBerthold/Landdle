55ebca909853e6651d57bc564e4e400ab7e3d348
MarkusBerthold <markus38@live.dk>
2024-09-19 22:05:30 +0200

    Countries

---
+++ Landdle/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace Landdle.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        // Navigations (To imply a junction table)
+        public List<Continent> Continents { get; } = [];
+    }
+}
